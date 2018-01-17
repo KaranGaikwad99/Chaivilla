956f5e72cc477d3a06092c3dfeb433c571efd81e
ajay <ajaymundhe21@gmail.com>
2018-01-17 15:42:49 +0530

    runtime

--- Chaivilla/urls.py
+++ Chaivilla/urls.py
@@ -24,7 +24,7 @@ from menu.views import Beverage_view
 from menu.views import Desserts_view
 from menu.views import sandwitch_view
 from home.views import Homepageview
-from menu.views import item
+from home.views import item
 #from home.views import BeverageView
 
 urlpatterns = [
@@ -36,6 +36,6 @@ urlpatterns = [
     url(r'^Beverage/',Beverage_view,name='Beverage_view'),        
     url(r'^MainCourse',MainCourse,name='MainCourse'),
     #url(r'^item/(?P<menu_id>[0-9]+)',item,name='item'),
-   url(r'^menu/item/(?P<menu_id>[\w-]+)/$',item,name='item'),
+   url(r'^item/(?P<menu_id>[\w-]+)/$',item,name='item'),
   #  url(r'beverages',BeverageView,name='BeverageView'),
 ]
--- home/views.py
+++ home/views.py
@@ -11,3 +11,12 @@ class Homepageview(TemplateView):
 class BeverageView(TemplateView):
 	template_name='beverages.html'
 
+def item(request, menu_id):
+	#template=loader.get_template('menu.html')	
+	queryset= Menu_model.objects.filter(category_types=menu_id)
+	#print queryset
+	context={
+	'items':queryset
+	}
+	#return render(template,render(context,request))
+	return render(request,"menu.html",context)
